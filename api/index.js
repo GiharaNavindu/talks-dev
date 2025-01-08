c0e59997b6bede93572b398b89b15499ea27cd5d
GiharaNavindu <raviherath1961@gamil.com>
2025-01-08 07:14:21 +0530

    shadcn ui

--- api/index.js
+++ api/index.js
@@ -1,3 +1,211 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cookieParser = require('cookie-parser');
+// const dotenv = require('dotenv');
+// const jwt = require('jsonwebtoken');
+// const cors = require('cors');
+// const bcrypt = require('bcryptjs');
+// const User = require('./models/User');
+// const Message = require('./models/Message');
+// const ws = require('ws');
+// const fs = require('fs');
+
+// dotenv.config();
+// mongoose.connect(process.env.MONGO_URL, (err) => {
+//   if (err) throw err;
+//   console.log('Connected to the database successfully!'); // Log for database connection
+// });
+// const jwtSecret = process.env.JWT_SECRET;
+// const bcryptSalt = bcrypt.genSaltSync(10);
+
+// const app = express();
+// app.use('/uploads', express.static(__dirname + '/uploads'));
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors({
+//   credentials: true,
+//   origin: process.env.CLIENT_URL,
+// }));
+
+// async function getUserDataFromRequest(req) {
+//   return new Promise((resolve, reject) => {
+//     const token = req.cookies?.token;
+//     if (token) {
+//       jwt.verify(token, jwtSecret, {}, (err, userData) => {
+//         if (err) throw err;
+//         resolve(userData);
+//       });
+//     } else {
+//       reject('no token');
+//     }
+//   });
+
+// }
+
+// app.get('/test', (req,res) => {
+//   res.json('test ok');
+// });
+
+// app.get('/messages/:userId', async (req,res) => {
+//   const {userId} = req.params;
+//   const userData = await getUserDataFromRequest(req);
+//   const ourUserId = userData.userId;
+//   const messages = await Message.find({
+//     sender:{$in:[userId,ourUserId]},
+//     recipient:{$in:[userId,ourUserId]},
+//   }).sort({createdAt: 1});
+//   res.json(messages);
+// });
+
+// app.get('/people', async (req,res) => {
+//   const users = await User.find({}, {'_id':1,username:1});
+//   res.json(users);
+// });
+
+// app.get('/profile', (req,res) => {
+//   const token = req.cookies?.token;
+//   if (token) {
+//     jwt.verify(token, jwtSecret, {}, (err, userData) => {
+//       if (err) throw err;
+//       res.json(userData);
+//       console.log("Token found in the cookie:", userData);
+//     });
+//   } else {
+//     res.status(401).json('no token');
+//     console.log("No token found in the cookie");
+//   }
+// });
+
+// app.post('/login', async (req,res) => {
+//   const {username, password} = req.body;
+//   const foundUser = await User.findOne({username});
+//   if (foundUser) {
+//     const passOk = bcrypt.compareSync(password, foundUser.password);
+//     if (passOk) {
+//       jwt.sign({userId:foundUser._id,username}, jwtSecret, {}, (err, token) => {
+//         res.cookie('token', token, {sameSite:'none', secure:true}).json({
+//           id: foundUser._id,
+//         });
+//       });
+//     }
+//   }
+// });
+
+// app.post('/logout', (req,res) => {
+//   res.cookie('token', '', {sameSite:'none', secure:true}).json('ok');
+// });
+
+// app.post('/register', async (req,res) => {
+//   const {username,password} = req.body;
+//   try {
+//     const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+//     const createdUser = await User.create({
+//       username:username,
+//       password:hashedPassword,
+//     });
+//     jwt.sign({userId:createdUser._id,username}, jwtSecret, {}, (err, token) => {
+//       if (err) throw err;
+//       res.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
+//         id: createdUser._id,
+//       });
+//     });
+//   } catch(err) {
+//     if (err) throw err;
+//     res.status(500).json('error');
+//   }
+// });
+
+// const server = app.listen(4040);
+
+// const wss = new ws.WebSocketServer({server});
+// wss.on('connection', (connection, req) => {
+
+//   function notifyAboutOnlinePeople() {
+//     [...wss.clients].forEach(client => {
+//       client.send(JSON.stringify({
+//         online: [...wss.clients].map(c => ({userId:c.userId,username:c.username})),
+//       }));
+//     });
+//   }
+
+//   connection.isAlive = true;
+
+//   connection.timer = setInterval(() => {
+//     connection.ping();
+//     connection.deathTimer = setTimeout(() => {
+//       connection.isAlive = false;
+//       clearInterval(connection.timer);
+//       connection.terminate();
+//       notifyAboutOnlinePeople();
+//       console.log('dead');
+//     }, 1000);
+//   }, 5000);
+
+//   connection.on('pong', () => {
+//     clearTimeout(connection.deathTimer);
+//   });
+
+//   // read username and id form the cookie for this connection
+//   const cookies = req.headers.cookie;
+//   if (cookies) {
+//     const tokenCookieString = cookies.split(';').find(str => str.startsWith('token='));
+//     if (tokenCookieString) {
+//       const token = tokenCookieString.split('=')[1];
+//       if (token) {
+//         jwt.verify(token, jwtSecret, {}, (err, userData) => {
+//           if (err) throw err;
+//           const {userId, username} = userData;
+//           connection.userId = userId;
+//           connection.username = username;
+//         });
+//       }
+//     }
+//   }
+
+//   connection.on('message', async (message) => {
+//     const messageData = JSON.parse(message.toString());
+//     const {recipient, text, file} = messageData;
+//     let filename = null;
+//     if (file) {
+//       console.log('size', file.data.length);
+//       const parts = file.name.split('.');
+//       const ext = parts[parts.length - 1];
+//       filename = Date.now() + '.'+ext;
+//       const path = __dirname + '/uploads/' + filename;
+//       const bufferData = new Buffer(file.data.split(',')[1], 'base64');
+//       fs.writeFile(path, bufferData, () => {
+//         console.log('file saved:'+path);
+//       });
+//     }
+//     if (recipient && (text || file)) {
+//       const messageDoc = await Message.create({
+//         sender:connection.userId,
+//         recipient,
+//         text,
+//         file: file ? filename : null,
+//       });
+//       console.log('created message');
+//       [...wss.clients]
+//         .filter(c => c.userId === recipient)
+//         .forEach(c => c.send(JSON.stringify({
+//           text,
+//           sender:connection.userId,
+//           recipient,
+//           file: file ? filename : null,
+//           _id:messageDoc._id,
+//         })));
+//     }
+//   });
+
+//   // notify everyone about online people (when someone connects)
+//   notifyAboutOnlinePeople();
+// });
+
+
+
+
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -22,9 +230,18 @@ const app = express();
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(express.json());
 app.use(cookieParser());
+
+// CORS configuration
 app.use(cors({
   credentials: true,
-  origin: process.env.CLIENT_URL,
+  origin: function (origin, callback) {
+    const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:5173'];
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
 }));
 
 async function getUserDataFromRequest(req) {
@@ -32,98 +249,109 @@ async function getUserDataFromRequest(req) {
     const token = req.cookies?.token;
     if (token) {
       jwt.verify(token, jwtSecret, {}, (err, userData) => {
-        if (err) throw err;
+        if (err) reject(err);
         resolve(userData);
       });
     } else {
       reject('no token');
     }
   });
-
 }
 
-app.get('/test', (req,res) => {
+app.get('/test', (req, res) => {
   res.json('test ok');
 });
 
-app.get('/messages/:userId', async (req,res) => {
-  const {userId} = req.params;
-  const userData = await getUserDataFromRequest(req);
-  const ourUserId = userData.userId;
-  const messages = await Message.find({
-    sender:{$in:[userId,ourUserId]},
-    recipient:{$in:[userId,ourUserId]},
-  }).sort({createdAt: 1});
-  res.json(messages);
+app.get('/messages/:userId', async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const userData = await getUserDataFromRequest(req);
+    const ourUserId = userData.userId;
+    const messages = await Message.find({
+      sender: { $in: [userId, ourUserId] },
+      recipient: { $in: [userId, ourUserId] },
+    }).sort({ createdAt: 1 });
+    res.json(messages);
+  } catch (error) {
+    res.status(401).json('Unauthorized');
+  }
 });
 
-app.get('/people', async (req,res) => {
-  const users = await User.find({}, {'_id':1,username:1});
+app.get('/people', async (req, res) => {
+  const users = await User.find({}, { '_id': 1, username: 1 });
   res.json(users);
 });
 
-app.get('/profile', (req,res) => {
+app.get('/profile', (req, res) => {
   const token = req.cookies?.token;
   if (token) {
     jwt.verify(token, jwtSecret, {}, (err, userData) => {
-      if (err) throw err;
+      if (err) {
+        console.error("JWT verification failed:", err);
+        return res.status(401).json('Invalid token');
+      }
       res.json(userData);
       console.log("Token found in the cookie:", userData);
     });
   } else {
-    res.status(401).json('no token');
+    res.status(401).json('No token provided');
     console.log("No token found in the cookie");
   }
 });
 
-app.post('/login', async (req,res) => {
-  const {username, password} = req.body;
-  const foundUser = await User.findOne({username});
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await User.findOne({ username });
   if (foundUser) {
     const passOk = bcrypt.compareSync(password, foundUser.password);
     if (passOk) {
-      jwt.sign({userId:foundUser._id,username}, jwtSecret, {}, (err, token) => {
-        res.cookie('token', token, {sameSite:'none', secure:true}).json({
+      jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (err, token) => {
+        res.cookie('token', token, { sameSite: 'none', secure: true }).json({
           id: foundUser._id,
         });
       });
+    } else {
+      res.status(401).json('Invalid password');
     }
+  } else {
+    res.status(404).json('User not found');
   }
 });
 
-app.post('/logout', (req,res) => {
-  res.cookie('token', '', {sameSite:'none', secure:true}).json('ok');
+app.post('/logout', (req, res) => {
+  res.cookie('token', '', { sameSite: 'none', secure: true }).json('ok');
 });
 
-app.post('/register', async (req,res) => {
-  const {username,password} = req.body;
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
   try {
     const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
     const createdUser = await User.create({
-      username:username,
-      password:hashedPassword,
+      username: username,
+      password: hashedPassword,
     });
-    jwt.sign({userId:createdUser._id,username}, jwtSecret, {}, (err, token) => {
+    jwt.sign({ userId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
       if (err) throw err;
-      res.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
+      res.cookie('token', token, { sameSite: 'none', secure: true }).status(201).json({
         id: createdUser._id,
       });
     });
-  } catch(err) {
-    if (err) throw err;
-    res.status(500).json('error');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('Error creating user');
   }
 });
 
-const server = app.listen(4040);
+const server = app.listen(4040, () => {
+  console.log('Server is running on port 4040');
+});
 
-const wss = new ws.WebSocketServer({server});
+const wss = new ws.WebSocketServer({ server });
 wss.on('connection', (connection, req) => {
-
   function notifyAboutOnlinePeople() {
     [...wss.clients].forEach(client => {
       client.send(JSON.stringify({
-        online: [...wss.clients].map(c => ({userId:c.userId,username:c.username})),
+        online: [...wss.clients].map(c => ({ userId: c.userId, username: c.username })),
       }));
     });
   }
@@ -145,7 +373,7 @@ wss.on('connection', (connection, req) => {
     clearTimeout(connection.deathTimer);
   });
 
-  // read username and id form the cookie for this connection
+  // Read username and id from the cookie for this connection
   const cookies = req.headers.cookie;
   if (cookies) {
     const tokenCookieString = cookies.split(';').find(str => str.startsWith('token='));
@@ -153,8 +381,11 @@ wss.on('connection', (connection, req) => {
       const token = tokenCookieString.split('=')[1];
       if (token) {
         jwt.verify(token, jwtSecret, {}, (err, userData) => {
-          if (err) throw err;
-          const {userId, username} = userData;
+          if (err) {
+            console.error("WebSocket token verification failed:", err);
+            return;
+          }
+          const { userId, username } = userData;
           connection.userId = userId;
           connection.username = username;
         });
@@ -164,39 +395,35 @@ wss.on('connection', (connection, req) => {
 
   connection.on('message', async (message) => {
     const messageData = JSON.parse(message.toString());
-    const {recipient, text, file} = messageData;
+    const { recipient, text, file } = messageData;
     let filename = null;
     if (file) {
       console.log('size', file.data.length);
       const parts = file.name.split('.');
       const ext = parts[parts.length - 1];
-      filename = Date.now() + '.'+ext;
+      filename = Date.now() + '.' + ext;
       const path = __dirname + '/uploads/' + filename;
-      const bufferData = new Buffer(file.data.split(',')[1], 'base64');
+      const bufferData = Buffer.from(file.data.split(',')[1], 'base64');
       fs.writeFile(path, bufferData, () => {
-        console.log('file saved:'+path);
+        console.log('File saved: ' + path);
       });
     }
     if (recipient && (text || file)) {
       const messageDoc = await Message.create({
-        sender:connection.userId,
+        sender: connection.userId,
         recipient,
         text,
         file: file ? filename : null,
       });
-      console.log('created message');
+      console.log('Created message');
       [...wss.clients]
         .filter(c => c.userId === recipient)
         .forEach(c => c.send(JSON.stringify({
           text,
-          sender:connection.userId,
+          sender: connection.userId,
           recipient,
           file: file ? filename : null,
-          _id:messageDoc._id,
         })));
     }
   });
-
-  // notify everyone about online people (when someone connects)
-  notifyAboutOnlinePeople();
 });
--- client/src/RegisterAndLoginForm.jsx
+++ client/src/RegisterAndLoginForm.jsx
@@ -1,47 +1,135 @@
-import {useContext, useState} from "react";
+// import axios from "axios";
+// import { useContext, useState } from "react";
+// import ParticleBackground from "./ParticleBackground.jsx";
+// import { UserContext } from "./UserContext.jsx";
+
+// export default function RegisterAndLoginForm() {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [isLoginOrRegister, setIsLoginOrRegister] = useState("login");
+//   const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
+
+//   async function handleSubmit(ev) {
+//     ev.preventDefault();
+//     const url = isLoginOrRegister === "register" ? "register" : "login";
+//     const { data } = await axios.post(url, { username, password });
+//     setLoggedInUsername(username);
+//     setId(data.id);
+//   }
+
+//   return (
+//     <div className="bg-blue-50 h-screen flex items-center relative overflow-hidden">
+//       <ParticleBackground />
+//       <form
+//         className="w-64 mx-auto mb-12 bg-white p-8 rounded-lg shadow-lg z-10 transform transition-all duration-300 hover:scale-105"
+//         onSubmit={handleSubmit}
+//       >
+//         <h2 className="text-2xl font-bold text-center mb-6 text-blue-500">
+//           {isLoginOrRegister === "register" ? "Register" : "Login"}
+//         </h2>
+//         <input
+//           value={username}
+//           onChange={(ev) => setUsername(ev.target.value)}
+//           type="text"
+//           placeholder="Username"
+//           className="block w-full rounded-sm p-2 mb-4 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
+//         />
+//         <input
+//           value={password}
+//           onChange={(ev) => setPassword(ev.target.value)}
+//           type="password"
+//           placeholder="Password"
+//           className="block w-full rounded-sm p-2 mb-6 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
+//         />
+//         <button
+//           type="submit"
+//           className="bg-blue-500 text-white block w-full rounded-sm p-2 hover:bg-blue-600 transition-all"
+//         >
+//           {isLoginOrRegister === "register" ? "Register" : "Login"}
+//         </button>
+//         <div className="text-center mt-4 text-sm text-gray-600">
+//           {isLoginOrRegister === "register" ? (
+//             <div>
+//               Already a member?
+//               <button
+//                 className="ml-1 text-blue-500 hover:text-blue-600 transition-all"
+//                 onClick={() => setIsLoginOrRegister("login")}
+//               >
+//                 Login here
+//               </button>
+//             </div>
+//           ) : (
+//             <div>
+//               Don't have an account?
+//               <button
+//                 className="ml-1 text-blue-500 hover:text-blue-600 transition-all"
+//                 onClick={() => setIsLoginOrRegister("register")}
+//               >
+//                 Register
+//               </button>
+//             </div>
+//           )}
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
 import axios from "axios";
-import {UserContext} from "./UserContext.jsx";
+import { useContext, useState } from "react";
+import { UserContext } from "./UserContext.jsx";
 
 export default function RegisterAndLoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLoginOrRegister, setIsLoginOrRegister] = useState('login');
-  const {setUsername:setLoggedInUsername, setId} = useContext(UserContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoginOrRegister, setIsLoginOrRegister] = useState("login");
+  const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
   async function handleSubmit(ev) {
     ev.preventDefault();
-    const url = isLoginOrRegister === 'register' ? 'register' : 'login';
-    const {data} = await axios.post(url, {username,password});
+    const url = isLoginOrRegister === "register" ? "register" : "login";
+    const { data } = await axios.post(url, { username, password });
     setLoggedInUsername(username);
     setId(data.id);
   }
   return (
     <div className="bg-blue-50 h-screen flex items-center">
       <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
-        <input value={username}
-               onChange={ev => setUsername(ev.target.value)}
-               type="text" placeholder="username"
-               className="block w-full rounded-sm p-2 mb-2 border" />
-        <input value={password}
-               onChange={ev => setPassword(ev.target.value)}
-               type="password"
-               placeholder="password"
-               className="block w-full rounded-sm p-2 mb-2 border" />
+        <input
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+          type="text"
+          placeholder="username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <input
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+          type="password"
+          placeholder="password"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
         <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
-          {isLoginOrRegister === 'register' ? 'Register' : 'Login'}
+          {isLoginOrRegister === "register" ? "Register" : "Login"}
         </button>
         <div className="text-center mt-2">
-          {isLoginOrRegister === 'register' && (
+          {isLoginOrRegister === "register" && (
             <div>
               Already a member?
-              <button className="ml-1" onClick={() => setIsLoginOrRegister('login')}>
+              <button
+                className="ml-1"
+                onClick={() => setIsLoginOrRegister("login")}
+              >
                 Login here
               </button>
             </div>
           )}
-          {isLoginOrRegister === 'login' && (
+          {isLoginOrRegister === "login" && (
             <div>
               Dont have an account?
-              <button className="ml-1" onClick={() => setIsLoginOrRegister('register')}>
+              <button
+                className="ml-1"
+                onClick={() => setIsLoginOrRegister("register")}
+              >
                 Register
               </button>
             </div>
