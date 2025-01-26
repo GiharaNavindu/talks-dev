253292dff8e1e2a4a3ad9f851c049fd1df6b4f4f
GiharaNavindu <raviherath1961@gamil.com>
2025-01-26 15:05:04 +0530

    docker setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+  
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow the server to be accessible externally
+    port: 5173,      // Specify the port
+    strictPort: true // Ensures Vite will fail if the port is already in use
+  },
 })
