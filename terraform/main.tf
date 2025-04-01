82a6a5824880bdced47d1ba7d2266dbd6936f97a
P.A.G.N.  Rathnaweeera <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2025-04-01 16:08:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-071226ecf16aa7d96"  # Free-tier Ubuntu AMI (us-east-1)
+  ami           = "ami-071226ecf16aa7d96"
   instance_type = "t2.micro"
-  key_name      = "chat-app-key"  # Change to your key pair name
+  key_name      = "chat-app-key"
 
   tags = {
     Name = "chat-app"
   }
 }
-
