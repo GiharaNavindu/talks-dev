7c66ed6ffcd6f4aa43e125be87686472a1d65440
GiharaNavindu <raviherath1961@gamil.com>
2025-04-04 06:45:31 +0530

    ip setup

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -18,7 +18,7 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket("ws://localhost:4040");
+    const ws = new WebSocket(process.env.REACT_APP_WS_URL || "ws://localhost:4040")
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
