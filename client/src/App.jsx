769ac642f1987eacea25f89313fe4db0ecaa82de
P.A.G.N.  Rathnaweeera <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2025-04-04 09:12:05 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:4040';
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://44.204.27.188/4040',;
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
